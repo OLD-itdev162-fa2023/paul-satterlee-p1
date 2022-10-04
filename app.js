7894fd2097710bdae1666d14e40d1271cd9f8648
sattep91 <paul.webdevguy@gmail.com>
2022-10-04 05:50:12 -0500

    connected to database server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -10,5 +10,12 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
   res.send('We are on posts');
 });
+
+//CONNECT TO DB
+mongoose.connect(
+  'mongodb+srv://paulwebdevguy:Tweezer$$4338@cluster0.egwsown.mongodb.net/?retryWrites=true&w=majority',
+() => {
+  console.log("connected to DB");
+})
 
 app.listen(3000);
