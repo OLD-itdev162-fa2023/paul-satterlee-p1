83d97511e8a25845f25f1c1ddaf44e6acb30c619
sattep91 <paul.webdevguy@gmail.com>
2022-10-04 20:45:33 -0500

    completed project

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+//middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 //ROUTES
 app.get('/', (req, res) => {
   res.send('We are on home');
 });
 
-app.get('/posts', (req, res) => {
-  res.send('We are on posts');
-});
+
 
 //CONNECT TO DB
 mongoose.connect(
-  'mongodb+srv://paulwebdevguy:Tweezer$$4338@cluster0.egwsown.mongodb.net/?retryWrites=true&w=majority',
+  process.env.DB_CONNECTION,
 () => {
   console.log("connected to DB");
 })
